2d875b615d40b8553c2dd4e9104949ea647caa9a
nezlobin118@gmail.com <nezlobin118@gmail.com>
2022-02-16 16:37:34 +0800

    add requirements.txt

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aiobalaboba import balaboba
 from sqlighter import SQLighter
 import random
 
-balabob = ''
+balabob = '' # В кавычки пишем запрос(то, что нужно продолжить)
 
 r = 10 # Количество строк результата в БД
 
@@ -27,7 +27,6 @@ async def main():
         response = await balaboba(balabob)
         db.add_result(response)
         count += 1
-        #print(response)
 
 
 if __name__ == "__main__":
