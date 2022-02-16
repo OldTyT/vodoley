8f0d55e10954298a82a597a3e351f55ec244d1d0
nezlobin118@gmail.com <nezlobin118@gmail.com>
2022-02-16 18:20:54 +0800

    edit

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ r = 10  # Количество строк результата в БД
 def rnd():
     chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
     length = 10
-    password = ''
+    randomized = ''
     for i in range(length):
-        password += random.choice(chars)
-    return password
+        randomized += random.choice(chars)
+    return randomized
 
 
 db = SQLighter(f'{rnd()}.db')
@@ -26,7 +26,7 @@ async def main():
     db.create_result()
     while True:
         if count >= r:
-            break;
+            break
         response = await balaboba(balabob)
         db.add_result(response)
         count += 1
