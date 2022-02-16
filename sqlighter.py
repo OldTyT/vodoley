a4733bf50e964e12923e91116657101097ef657b
nezlobin118@gmail.com <nezlobin118@gmail.com>
2022-02-16 17:14:50 +0800

    add README.md

--- sqlighter.py
+++ sqlighter.py
@@ -9,19 +9,17 @@ class SQLighter:
         self.cursor = self.connection.cursor()
 
     def create_result(self):
-        """Проверяем, есть ли уже пост в базе"""
+        """Создаем бд"""
         with self.connection:
             self.cursor.execute("""
                                                             CREATE TABLE "results" (
                                                                 "id"	INTEGER,
                                                                 "text"	TEXT,
-                                                                "no_text"	TEXT,
                                                                 PRIMARY KEY("id" AUTOINCREMENT)
                                                             )""")
         return False
 
     def add_result(self, text):
-        """Добавляем новый пост в БД"""
+        """Добавляем новую запись в БД"""
         with self.connection:
-            return self.cursor.execute("INSERT INTO `results` (`text`, `no_text`) VALUES(?,?)", (text, 1))
-            #return self.cursor.execute("INSERT INTO `results` (`text`,) VALUES(?,)", (text, 1))
+            return self.cursor.execute("INSERT INTO `results` (`text`) VALUES(?)", (text,))
